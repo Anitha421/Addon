1ff1952b449a903cd29020362c07cb84a9862968
Anitha421 <anithasankar421@gmail.com>
2025-03-25 15:25:55 +0530

    session6

---
+++ session6/ExceptionEx1.java
@@ -0,0 +1,26 @@
+package session5;
+
+public class ExceptionEx1 {
+          
+	String str1="123";//instance variable
+	public void name(){
+		
+		try {
+			
+			int i = Integer.parseInt(str1);
+			System.out.println("the value of converted String:" +str1);
+			
+		} catch(Exception e) {
+			
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+		
+}
+	
+	public static void main(String[] args) {
+		
+		ExceptionEx1 ee = new ExceptionEx1();
+		ee.name();
+	}
+}
